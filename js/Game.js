23a70cf1247eb459bb47dcc8d02fdcb83fb05ea4
Samarth <misrarashi30@gmail.com>
2021-03-04 20:08:27 +0530

    This is a message

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1Img);
     car2 = createSprite(300,200);
+    car2.addImage(car2Img);
     car3 = createSprite(500,200);
+    car3.addImage(car3Img);
     car4 = createSprite(700,200);
+    car4.addImage(car4Img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -40,15 +44,16 @@ class Game {
     form.hide();
 
     Player.getPlayerInfo();
-    
+    //GAME STARTS
     if(allPlayers !== undefined){
-      //var display_position = 100;
-      
+      background("Green");
+      image(track, 0, -4*displayHeight, displayWidth, displayHeight*5);
+     
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 150;
       var y;
 
       for(var plr in allPlayers){
@@ -79,6 +84,20 @@ class Game {
       player.update();
     }
 
+
+    if(player.distance>3438){
+      gameState = 2;
+    }
     drawSprites();
   }
+
+end(){
+  console.log("Congrats to the winner and the other people should get lost and never play this game again. Nothing for the winner :|")
+}
+
+
+
+
+
+
 }
